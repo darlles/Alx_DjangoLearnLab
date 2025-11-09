fb84e0c943928012246e922afb893c68e1bcaf68
Alexander <bizzle.teye@gmail.com>
2025-11-09 20:46:35 +0000

    com

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,8 @@ from .models import Book
 # Create your views here.
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 from django.views.generic.detail import DetailView
 from .models import Library
