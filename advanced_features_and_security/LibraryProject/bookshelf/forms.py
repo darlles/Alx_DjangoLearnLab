e4ba5c788f2bc60371a8825ece558246bf2c4379
Alexander <bizzle.teye@gmail.com>
2025-11-16 23:09:22 +0000

    Your commit message

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,11 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     title = forms.CharField(max_length=100, required=False)
+
+    from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']  # adjust based on your actual model fields
